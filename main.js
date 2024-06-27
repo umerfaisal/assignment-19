96a9941aa42c14b1b86748578b53a2fe8c7eb1b5
umerfaisal <109797391+umerfaisal@users.noreply.github.com>
2024-06-27 17:34:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var guestList = ["Umer", "Mubeen", "Huzaifa", "Areeb"];
+var lenghtGuests = guestList.length;
+console.log("we are inviting total ".concat(lenghtGuests, " guests"));
+//guestList.forEach(oneGuest => console.log(`hello ${oneGuest}, Would you like to dinner with me? `))
---
+++ main.ts
@@ -0,0 +1,6 @@
+let guestList = ["Umer","Mubeen","Huzaifa","Areeb"]
+
+let lenghtGuests : number = guestList.length;
+
+console.log(`we are inviting total ${lenghtGuests} guests`);
+
